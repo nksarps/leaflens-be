98034abaa2eec39ddf3fede09f6b8321d44f1bea
kweku-xvi <nksarps@gmail.com>
2025-05-12 09:50:39 +0000

    added user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,51 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        extra_fields.setdefault("is_verified",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+        if extra_fields.get("is_verified") is not True:
+            raise ValueError(_("SuperUser must have is_verified=True"))
+
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        ordering = ('-created_at',)
