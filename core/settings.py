b3b6680db16dc7e4358c0cfcc134ac3f91246899
kweku-xvi <nksarps@gmail.com>
2025-05-12 09:50:01 +0000

    added accounts to installed apps

--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SETTING USER MODEL 
+AUTH_USER_MODEL = 'accounts.User'
