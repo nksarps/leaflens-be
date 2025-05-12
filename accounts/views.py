ba8757e1d1c90a73b2494f78436c2f6ee8cc9648
kweku-xvi <nksarps@gmail.com>
2025-05-12 09:50:16 +0000

    added sign up view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
+from accounts.serializers import SignUpSerializer
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def sign_up(request):
+    if request.method == 'POST':
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response({
+                'success':True,
+                'user':serializer.data
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response({
+            'success':False,
+            'message':serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
