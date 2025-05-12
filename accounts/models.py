96aab7b4c53b80cda53c21f67b43b4697d95c24a
kweku-xvi <nksarps@gmail.com>
2025-05-12 09:56:08 +0000

    refactor models

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class MyUserManager(BaseUserManager):
