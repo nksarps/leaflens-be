686ce99027ac080063a32a56448dd293eb0c81f2
Nana Kwaku <nksarps@gmail.com>
2025-05-14 09:20:02 +0000

    added url handlers for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('signup/', views.sign_up, name='sign_up'),
     path('login/', views.log_in, name='log_in'),
-    path('verify-user/', views.verify_user, name='verify_user')
+    path('verify-user/', views.verify_user, name='verify_user'),
+    path('password-reset/', views.password_reset, name='password_reset'),
+    path('password-reset-confirm/', views.password_reset_confirm, name='password_reset_confirm'),
 ]
