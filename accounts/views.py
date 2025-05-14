54fd496d4a79923b6501736b5ab9f705100eaada
Nana Kwaku <nksarps@gmail.com>
2025-05-14 08:17:35 +0000

    refactor sign up view and added view for user account confirmation

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,16 @@
+import os, jwt
+from accounts.models import User
 from accounts.serializers import SignUpSerializer, LogInSerializer
+from accounts.utils import send_confirmation_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from dotenv import load_dotenv
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+load_dotenv()
 
 
 @api_view(['POST'])
@@ -12,15 +21,77 @@ def sign_up(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
+            user = User.objects.get(email=serializer.validated_data['email'])
+            token = RefreshToken.for_user(user)
+            current_site = get_current_site(request).domain
+            relative_link = reverse('verify_user')
+            absolute_url = f'http://{current_site}{relative_link}?token={token}'
+            link = str(absolute_url)
+            send_confirmation_mail(email=user.email, first_name=user.first_name, link=link)
+
             return Response({
                 'success':True,
-                'user':serializer.data
+                'message':'Email has been sent to the address you provided. Verify email account to log in.'
             }, status=status.HTTP_201_CREATED)
         
         return Response({
             'success':False,
             'message':serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET'])
+def verify_user(request):
+    if request.method == 'GET':
+        token = request.GET.get('token')
+
+        try:
+            payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+            user = User.objects.get(id=payload['user_id'])
+
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+
+            return Response({
+                'success':True,
+                'message':'Email verified successfully'
+            }, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Activation link expired'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.exceptions.DecodeError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Invalid token'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.exceptions.InvalidTokenError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Invalid token'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except User.DoesNotExist as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'User not found'
+                }, status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':str(e)
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 @api_view(['POST'])
