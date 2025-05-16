567254f4f7dbcecaef88178154b8722d22aec2da
Nana Kwaku <nksarps@gmail.com>
2025-05-16 09:33:09 +0000

    added model for messages

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,17 @@
+from accounts.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session_id = models.CharField(max_length=255, db_index=True)
+    sender = models.CharField(max_length=25)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.sender} ({self.session_id}): {self.message[:50]}'
+
+    class Meta:
+        ordering = ('timestamp',)
