0239d729d2b861804ef01ad0e39a01988459a9fa
Nana Kwaku <nksarps@gmail.com>
2025-05-16 09:34:08 +0000

    added views for starting and continuing chat

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,125 @@
+import os, uuid
+import google.generativeai as genai
+from accounts.permissions import IsVerified
+from chatbot.models import Chat
 from django.shortcuts import render
+from dotenv import load_dotenv
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+@api_view(['POST'])
+@permission_classes([IsVerified])
+def start_chat(request):
+    if request.method == 'POST':
+        user = request.user
+
+        user_msg = request.data.get('message')
+
+        if not user_msg:
+            return Response({
+                'success':False,
+                'message':'Message is required to start chat'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        session_id = str(uuid.uuid4())
+
+        try:
+            chat = model.start_chat()
+
+            response = chat.send_message(user_msg)
+            bot_resp = response.text
+
+            Chat.objects.create(
+                user=user,
+                session_id=session_id,
+                sender='user',
+                message=user_msg
+            )
+
+            Chat.objects.create(
+                user=user,
+                session_id=session_id,
+                sender='model',
+                message=bot_resp
+            )
+
+            return Response({
+                'success':True,
+                'session_id':session_id,
+                'response':bot_resp
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(f"Error processing chat: {e}")
+            return Response({
+                'success':False,
+                'message':f'An error occurred: {str(e)}'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+
+@api_view(['POST'])
+@permission_classes([IsVerified]) # Change to IsVerified
+def continue_chat(request, session_id:str):
+    if request.method == 'POST':
+        user = request.user
+
+        if not Chat.objects.filter(session_id=session_id, user=user).exists():
+            return Response({
+                'success':False,
+                'message':'Session ID not found. Start a new chat.'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        user_msg = request.data.get('message')
+
+        if not user_msg:
+            return Response({
+                'success':False,
+                'message':'Message is required'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            chat_history_db = Chat.objects.filter(session_id=session_id)
+
+            history_for_gemini = []
+            for msg in chat_history_db:
+                role = 'user' if msg.sender == 'user' else 'model'
+                history_for_gemini.append({'role':role, 'parts':[msg.message]})
+
+            chat = model.start_chat(history=history_for_gemini)
+
+            response = chat.send_message(user_msg)
+            bot_resp = response.text
+
+            Chat.objects.create(
+                user=user,
+                session_id=session_id,
+                sender='user',
+                message=user_msg
+            )
+
+            Chat.objects.create(
+                user=user,
+                session_id=session_id,
+                sender='model',
+                message=bot_resp
+            )
+
+            return Response({
+                'success':True,
+                'session_id':session_id,
+                'response':bot_resp
+            }, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            print(f"Error processing chat: {e}")
+            return Response({
+                'success':False,
+                'message':f'An error occurred: {str(e)}'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        
