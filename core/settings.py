61900597ad0c7006032f4449e881977d10aa3c36
Nana Kwaku <nksarps@gmail.com>
2025-05-19 11:06:54 +0000

    added profiles to installed apps

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles',
     'rest_framework',
     'rest_framework_simplejwt'
 ]
