09cf4f8c3ccf0d9497522a46a0a71320385d8e53
Nana Kwaku <nksarps@gmail.com>
2025-05-19 09:05:45 +0000

    refactored code

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -2,6 +2,6 @@ from chatbot.models import Chat
 from django.contrib import admin
 
 class ChatAdmin(admin.ModelAdmin):
-    list_display = ('session_id', 'user')
+    list_display = ('session_id', 'sender', 'timestamp')
 
 admin.site.register(Chat, ChatAdmin)
