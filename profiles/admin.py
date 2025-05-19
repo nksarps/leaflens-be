57197dbbe8f7c7789bd9a9087feded6e48eeea18
Nana Kwaku <nksarps@gmail.com>
2025-05-19 12:02:53 +0000

    added profiles to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from profiles.models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+
+admin.site.register(Profile, ProfileAdmin)
