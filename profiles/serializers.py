0835a6fa4a6f199017fcc861763587152fc9acf8
Nana Kwaku <nksarps@gmail.com>
2025-05-23 08:46:09 +0000

    added profiles serializer

---
+++ profiles/serializers.py
@@ -0,0 +1,14 @@
+from accounts.serializers import UserSerializer
+from profiles.models import Profile
+from rest_framework import serializers
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'image', 'bio', 'phone_number', 'location']
+
+        read_only_fields = ['id', 'user']
+
