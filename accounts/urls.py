ab4b54a5ac2b0223d9f104e7f875f771eae8867c
Nana Kwaku <nksarps@gmail.com>
2025-06-03 08:48:00 +0000

    added url endpoint to edit user information

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('signup/', views.sign_up, name='sign_up'),
     path('login/', views.log_in, name='log_in'),
     path('verify-user/', views.verify_user, name='verify_user'),
+    path('update-info/', views.update_user_info, name='update_user_info'),
     path('password-reset/', views.password_reset, name='password_reset'),
     path('password-reset-confirm/', views.password_reset_confirm, name='password_reset_confirm')
 ]
