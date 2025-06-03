fca16dd6d0fefabbf24d205fdf37eb5742291bbe
Nana Kwaku <nksarps@gmail.com>
2025-06-03 09:55:27 +0000

    added url endpoint to get all predictions

--- predict/urls.py
+++ predict/urls.py
@@ -3,5 +3,6 @@ from predict import views
 
 
 urlpatterns = [
-    path('', views.predict_disease, name='predict_disease')
+    path('', views.predict_disease, name='predict_disease'),
+    path('all/', views.get_all_predictions, name='get_all_predictions')
 ]
