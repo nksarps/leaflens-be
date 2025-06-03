082d24a8702a4292cccb76a12e08797407342de2
Nana Kwaku <nksarps@gmail.com>
2025-06-03 08:13:05 +0000

    removed bio from profile model

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,6 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_images/', blank=True, null=True) # Add default profile
-    bio = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
