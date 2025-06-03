1e1ca286ca7d80954c358446bc692bbaed1106ec
Nana Kwaku <nksarps@gmail.com>
2025-06-03 09:54:22 +0000

    added model to save predictions

--- predict/models.py
+++ predict/models.py
@@ -1,3 +1,17 @@
+from accounts.models import User
 from django.db import models
 
-# Create your models here.
+
+class DiseasePrediction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='predictions/')
+    prediction = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.prediction
+    
+
+    class Meta:
+        ordering = ('-timestamp',)
