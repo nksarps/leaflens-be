d747f056a5f9d90a58f0121efba3b8927e73ec84
Nana Kwaku <nksarps@gmail.com>
2025-06-03 07:59:52 +0000

    added url endpoints to successfully delete chat sessions

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.start_chat, name='start_new_chat'),
     path('history/', views.get_user_chat_history, name='get_user_chat_history'),
     path('<str:session_id>/', views.continue_chat, name='continue_chat'),
+    path('<str:session_id>/delete/', views.delete_chat_session, name='delete_chat_session'),
 ]
