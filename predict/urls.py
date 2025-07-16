9648459ee290c34b8116ae6716b56c4133f9a71b
Nana Kwaku <nksarps@gmail.com>
2025-07-16 05:08:55 +0000

    added url endpoints to get and delete predictions

--- predict/urls.py
+++ predict/urls.py
@@ -4,5 +4,7 @@ from predict import views
 
 urlpatterns = [
     path('', views.predict_disease, name='predict_disease'),
-    path('all/', views.get_all_predictions, name='get_all_predictions')
+    path('all/', views.get_all_predictions, name='get_all_predictions'),
+    path('<str:id>/', views.get_prediction, name='get_prediction'),
+    path('<str:id>/delete/', views.delete_prediction, name='delete_prediction'),
 ]
