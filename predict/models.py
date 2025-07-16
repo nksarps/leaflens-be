9611aa3d6032df45876fbe0f1bf16369c3a8a1a5
Nana Kwaku <nksarps@gmail.com>
2025-07-16 05:08:00 +0000

    added updated id field in predictions model

--- predict/models.py
+++ predict/models.py
@@ -1,8 +1,10 @@
+import uuid
 from accounts.models import User
 from django.db import models
 
 
 class DiseasePrediction(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='predictions/')
     prediction = models.CharField(max_length=50)
