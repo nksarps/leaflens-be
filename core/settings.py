3eb34488882dd3242698c086de01bfcde93ce2c9
Nana Kwaku <nksarps@gmail.com>
2025-07-16 06:06:23 +0000

    added configuration settings for api documentation with drf yasg

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'chatbot',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
