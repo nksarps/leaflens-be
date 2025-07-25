a3969e4215145ed57622a3414e6e2ad9e8dc7143
Nana Kwaku <nksarps@gmail.com>
2025-07-25 04:47:26 +0000

    made changes to cors config

--- core/settings.py
+++ core/settings.py
@@ -156,26 +156,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # CORSHEADERS SETTINGS
 # Allow CORS requests from any origin
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_CREDENTIALS = True
-
-CORS_ALLOWED_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with'
-]
-
-CORS_ALLOWED_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT'
-]
