2f83ffa5959f047cfa12c19b1b7212cfcd763cc5
Nana Kwaku <nksarps@gmail.com>
2025-07-31 13:51:13 +0000

    added url endpoint to get current user

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('verify-user/', views.verify_user, name='verify_user'),
     path('update-info/', views.update_user_info, name='update_user_info'),
     path('password-reset/', views.password_reset, name='password_reset'),
-    path('password-reset-confirm/', views.password_reset_confirm, name='password_reset_confirm')
+    path('password-reset-confirm/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('user/', views.get_current_user, name='get_current_user')
 ]
